ca2512cfcd4c0fadb1e561b79ad46e5f9e40336b
CatalinaML <listecatalina7@gmail.com>
2023-08-07 12:00:08 -0300

    Update Profile.js
    ver la lista de seguidores y seguidos desde el perfil del blogger

--- client/src/components/blogger/profile/Profile/Profile.js
+++ client/src/components/blogger/profile/Profile/Profile.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Button, Icon, Image, Label, LabelDetail } from "semantic-ui-react";
+import { Button, Icon, Image } from "semantic-ui-react";
 import { image } from "../../../../assets";
 
 import { ProfileForm } from "../FormProfile";
-import { Modal } from "../../../shared";
+import { Modal, ModalUsers } from "../../../shared";
 import { useAuth } from "../../../../hooks";
 import { ENV } from "../../../../utils";
 import { User } from "../../../../api";
+import { FollowerList } from "../../../shared/followersList/FollowerList";
 
 import "./Profile.scss";
 
@@ -20,9 +21,15 @@ export function Profile(props) {
 
   //MODAL
   const [showModal, setShowModal] = useState(false);
+  const [showFollowersModal, setShowFollowersModal] = useState(false);
+  const [type, setType] = useState(null);
 
   //ABRIR/CERRAR MODAL
   const onOpenCloseModal = () => setShowModal((prevState) => !prevState);
+  const onOpenCloseFollowersModal = (type) => {
+    setType(type);
+    setShowFollowersModal((prevState) => !prevState);
+  };
 
   useEffect(() => {
     (async () => {
@@ -55,14 +62,18 @@ export function Profile(props) {
           <h1>{user?.username}</h1>
 
           <div className="followers">
-            <Label size="medium">
-              Seguidos
-              <LabelDetail>{user?.follow.length || 0}</LabelDetail>
-            </Label>
-            <Label size="medium">
-              Seguidores
-              <LabelDetail>{user?.followers.length || 0}</LabelDetail>
-            </Label>
+            <Button
+              className="follow"
+              onClick={() => onOpenCloseFollowersModal("follow")}
+            >
+              {`${user?.follow.length || 0} Seguidos`}
+            </Button>
+            <Button
+              className="followers"
+              onClick={() => onOpenCloseFollowersModal("followers")}
+            >
+              {`${user?.followers.length || 0} Seguidores`}
+            </Button>
           </div>
 
           <div className="box-buttons">
@@ -75,6 +86,15 @@ export function Profile(props) {
           </div>
         </div>
       </div>
+
+      <ModalUsers
+        show={showFollowersModal}
+        close={() => onOpenCloseFollowersModal("")}
+        title={type === "follow" ? "Seguidos" : "Seguidores"}
+        size="tiny"
+      >
+        <FollowerList type={type} userProfile={user} />
+      </ModalUsers>
 
       <Modal
         show={showModal}
