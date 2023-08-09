2108c375f0f9e64ad302ef4eb5b5772d326926d9
CatalinaML <listecatalina7@gmail.com>
2023-08-09 15:22:08 -0300

    assets

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,15 +1,22 @@
 import { ReactComponent as LogoBook } from "./svg/logo-book.svg";
 import banner from "./jpg/banner.jpg";
 import noAvatar from "./jpg/avatar-default.jpg";
-import noImage from "./jpg/noImage.jpg"
-import logo from "./jpg/UniVerse.jpg"
+import noImage from "./jpg/noImage.jpg";
+import logo from "./jpg/UniVerse.jpg";
+import blogBg from "./jpg/blog-bg.jpg";
+import UniverseLogo from "./png/UniVerse.png";
 
 const image = {
-    banner, noAvatar, noImage, logo
+  UniverseLogo,
+  banner,
+  noAvatar,
+  noImage,
+  logo,
+  blogBg,
 };
 
 const Icon = {
-    LogoBook,
+  LogoBook,
 };
 
-export{image, Icon};
+export { image, Icon };
