b97850980bad152f88cb96393d8f503d300ea7bc
CatalinaML <listecatalina7@gmail.com>
2023-08-09 17:53:35 -0300

    update

--- server/index.js
+++ server/index.js
@@ -1,22 +1,30 @@
 const app = require("./App");
 const mongoose = require("mongoose");
-const {IP_SERVER, API_VERSION ,DB_HOST, DB_PASSWORD, DB_USER} = require("./utils/constants");
+const {
+  IP_SERVER,
+  API_VERSION,
+  DB_HOST,
+  DB_PASSWORD,
+  DB_USER,
+} = require("./utils/constants");
 
-const port = process.env.POST || 4000;
+const port = process.env.POST || 8080;
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/`);
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/`
+    );
 
-        app.listen(port, () => {
-            console.log("################");
-            console.log("CONEXIÓN EXITOSA");
-            console.log("################");
-            console.log(`http://${IP_SERVER}:${port}/api/${API_VERSION}/`);;
-        })
-    } catch (error) {
-        console.error(error);
-    }
-}
+    app.listen(port, () => {
+      console.log("################");
+      console.log("CONEXIÓN EXITOSA");
+      console.log("################");
+      console.log(`http://${IP_SERVER}:${port}/api/${API_VERSION}/`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 connectDB();
